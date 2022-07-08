f809c6c2b0a91a1879d514f4e1bf298b6a9bc377
Jamie Vital <jvital2013@gmail.com>
2022-07-08 12:24:17 -0400

    Add missing license notices

--- html/functions.php
+++ html/functions.php
@@ -1,4 +1,21 @@
 <?php
+/* 
+ * Copyright 2022 Jamie Vital
+ * This software is licensed under the GNU General Public License
+ * 
+ * This file is part of Vitality GOES.
+ * Vitality GOES is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Vitality GOES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with Vitality GOES.  If not, see <http://www.gnu.org/licenses/>.
+ */
 function loadConfig()
 {
 	$config = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . "/config/config.ini", true, INI_SCANNER_RAW);
