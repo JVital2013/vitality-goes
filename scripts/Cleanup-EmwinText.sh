886b92cfc76c72d7779a72b321e415a5305d24fd
Jamie Vital <jvital2013@gmail.com>
2022-07-21 18:00:13 -0400

    Support subdirectories in the Cleanup-EmwinText.sh script

--- scripts/Cleanup-EmwinText.sh
+++ scripts/Cleanup-EmwinText.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-zipStr="$(date --date="yesterday" +"%Y-%m-%d").zip"
-blobStart="*$(date --date="yesterday" +"%Y%m%d")"
-cd /path/to/goestoolsrepo/emwin
-
-zip $zipStr $(echo $blobStart)0*.TXT
-zip $zipStr $(echo $blobStart)1*.TXT
-zip $zipStr $(echo $blobStart)2*.TXT
-rm $(echo $blobStart)0*.TXT
-rm $(echo $blobStart)1*.TXT
-rm $(echo $blobStart)2*.TXT
+source "$(dirname "$(readlink -fm "$0")")/scriptconfig.ini"
+yesterdayEmwinText=$(find "$emwinSrcDir" -type f -name "*_$(date --date="yesterday" +"%Y%m%d")*.TXT")
+echo $yesterdayEmwinText | xargs zip -j "$emwinSrcDir/$(date --date="yesterday" +"%Y-%m-%d").zip"
+echo $yesterdayEmwinText | xargs rm
