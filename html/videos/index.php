61165a1dfe5d887da2d2680cbe7c83eb0bc91a2b
Jamie Vital <jvital2013@gmail.com>
2022-12-31 15:26:58 -0500

    Cleanup comment

--- html/videos/index.php
+++ html/videos/index.php
@@ -1,7 +1,7 @@
 <?php
 // This directory is designed to hold timelapse videos
-// Timelapses can be generated with the CreateVideos-ABI.sh
-// and CreateVideos-EMWIN.sh scripts included with Vitality
+// Timelapses can be generated with the CreateVideos-ABI
+// and CreateVideos-EMWIN scripts included with Vitality
 // GOES, or any other script you may have. Put the video
 // file name in your abi.ini, l2.ini, meso.ini, and emwin.ini
 // files as appropriate.
