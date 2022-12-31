ba58a4b9ffe1ce5dc869bb9725bc7d4a8ecc88b1
Jamie Vital <jvital2013@gmail.com>
2022-12-31 15:23:51 -0500

    Create Windows (PS) versions of important secondary scripts

---
+++ scripts-windows/Cleanup-EmwinText.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module "$PSScriptRoot\Functions.psm1"
+$config = Get-Config
+
+$yesterday = $(Get-Date).AddDays(-1)
+$yesterdayEmwinText = Get-ChildItem -Attributes !Directory -Recurse "$($config.emwinSrcDir)\*$($yesterday.ToString("yyyyMMdd"))*.TXT"
+Compress-Archive -Path $yesterdayEmwinText -DestinationPath "$($config.emwinSrcDir)\$($yesterday.ToString("yyyy-MM-dd")).zip"
+foreach($file in $yesterdayEmwinText)
+{
+    Remove-Item $file.FullName -Force | Out-Null
+}
