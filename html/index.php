ee8fe6be0cc70349cdb2ea37f9ab126dd7297663
Jamie Vital <jvital2013@gmail.com>
2023-01-07 14:22:55 -0500

    Change viewport and tweak light theme

--- html/index.php
+++ html/index.php
@@ -43,7 +43,7 @@ if($theme !== false)
 	<head>
 		<title><?php echo $siteTitle ?></title>
 
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover" />
 		<meta name="mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-capable" content="yes">
 
