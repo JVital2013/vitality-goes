6a36616c9148f809ed0fa298b5f68fa4020188ad
Jamie Vital <jvital2013@gmail.com>
2023-02-10 20:05:52 -0500

    Fix Cleanup-EmwinText.sh when extension is lower case

--- scripts-linux/Cleanup-EmwinText.sh
+++ scripts-linux/Cleanup-EmwinText.sh
@@ -6,6 +6,6 @@ then
 fi
 
 source "$(dirname "$(readlink -fm "$0")")/scriptconfig.ini"
-yesterdayEmwinText=$(find "$emwinSrcDir" -type f -name "*_$(date --date="yesterday" +"%Y%m%d")*.TXT")
+yesterdayEmwinText=$(find "$emwinSrcDir" -type f -iname "*_$(date --date="yesterday" +"%Y%m%d")*.TXT")
 echo $yesterdayEmwinText | xargs zip -j "$emwinSrcDir/$(date --date="yesterday" +"%Y-%m-%d").zip"
 echo $yesterdayEmwinText | xargs rm
